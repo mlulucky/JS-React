974a7d3f3e4e5848dcfbcd94c04fb990d5d2ccba
mlulucky <dmswjdans@gmail.com>
2023-09-24 17:00:15 +0900

    application_API&fetch

---
+++ 2.js_application/10.API&fetch.js
@@ -0,0 +1,22 @@
+// API 호출 == 누구에게 말을 거는 것(데이터 요청/응답)
+
+// https://jsonplaceholder.typicode.com/posts
+
+// fetch("URL")
+// API 호출을 도와주는 자바스크립트 내장함수 
+// Promise 를 반환하는 함수 
+// 비동기 처리하는 함수
+// 처리 결과는 then 을 통해서 사용가능 // then((API 호출 응답결과) => {}) 
+
+// fetch() => API 성공 객체(결과값의 포장지)를 반환
+// let result = fetch("https://jsonplaceholder.typicode.com/posts")
+// .then((res)=> console.log(res)); 
+
+// JSON 반환
+async function getData(){
+	let promiseResponse = await fetch("https://jsonplaceholder.typicode.com/posts");
+	let jsonResponse = await promiseResponse.json();
+	console.log(jsonResponse);
+}
+
+getData(); // 함수실행
