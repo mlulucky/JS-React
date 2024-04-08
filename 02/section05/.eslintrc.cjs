f247a35955941b56a6fe7280bb2f664e13bde572
mlulucky <dmswjdans@gmail.com>
2024-04-09 02:13:46 +0900

    create reactapp

---
+++ 02/section05/.eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+		"no-unused-vars": "off", // 사용안하는 변수 에러사항 옵션끄기
+		"react/prop-types": "off" 
+  },
+}
